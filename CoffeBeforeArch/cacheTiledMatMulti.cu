5392516a756e0720132640394866619b2b82192b
Kajetan <kajtek.gdynia@gmail.com>
2024-04-14 19:17:24 +0200

    .

--- CoffeBeforeArch/cacheTiledMatMulti.cu
+++ CoffeBeforeArch/cacheTiledMatMulti.cu
@@ -1,49 +1,122 @@
-#include <cuda_runtime.h>
-#include <device_launch_parameters.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <assert.h>
-#include <math.h>
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <vector>
 
-// Static shmem calculation for convenience (int 16x16 matrix)
-#define SHMEM_SIZE 16 * 16 * 4
+using std::cout;
+using std::generate;
+using std::vector;
 
-__global__ void titledMatrixMul(int *a, int *b, int *c, int n, int tile_size) {
-    // Two statistically-sized pieces of shared memory
-    __shared__ int A[SHMEM_SIZE];
-    __shared__ int B[SHMEM_SIZE];
+// Pull out matrix and shared memory tile size 
+const int N = 1 << 10;
+const int SHMEM_SIZE = 1 << 10;
 
-    // Shorten these parameters for clean re-use
-    int tx = threadIdx.x;
-    int ty = threadIdx.y;
-    int bx = blockIdx.x;
-    int by = blockIdx.y;
+__global__ void matrixMul(const int *a, const int *b, int *c) {
+  // Compute each thread's global row and column index
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
 
-    // Calculate global row and column positions for this thread
-    int row = by * tile_size + ty;
-    int col = bx * tile_size + tx;
+  // Statically allocated shared memory
+  __shared__ int s_a[SHMEM_SIZE];
+  __shared__ int s_b[SHMEM_SIZE];
 
-    // Intermediate sum for element being written
-    int temp_val = 0;
+  // Accumulate in temporary variable
+  int tmp = 0;
 
-    // Sweep tiles over entire matrix
-    for (int i = 0; i < (n / tile_size); i++) {
-        /*
-            Every thread in a threadb,ock loads one element into shared memory.
-            The elemetn location in shared memory corresponds to the thread's 
-            position in the threadblock (e.g. thread[0, 0] loads for
-            A[0 * tile_size + 0], and B[0 * tile_size + 0],)
+  // Sweep tile across matrix
+  for (int i = 0; i < N; i += blockDim.x) {
+    // Load in elements for this tile
+    s_a[threadIdx.y * blockDim.x + threadIdx.x] = a[row * N + i + threadIdx.x];
+    s_b[threadIdx.y * blockDim.x + threadIdx.x] =
+        b[i * N + threadIdx.y * N + col];
 
-             Explanation of indexing parameters
-             For A:
-                row*n: Indexes the global row for this thread (loop-invariant)
-                i*tile_size: Indexes the new set of colunns each iteration
-                tx: Indexes the column within that set
-            For B:
-                i*tile_size*n: INdexes the next set of rows each iteration
-                ty*n: Indexes the row within that set
-                col: Indexes the global column (loop_invariant)
-        */
-       B
+    // Wait for both tiles to be loaded in before doing computation
+    __syncthreads();
+
+    // Do matrix multiplication on the small matrix
+    for (int j = 0; j < blockDim.x; j++) {
+      tmp +=
+          s_a[threadIdx.y * blockDim.x + j] * s_b[j * blockDim.x + threadIdx.x];
     }
+
+    // Wait for all threads to finish using current tiles before loading in new
+    // ones
+    __syncthreads();
+  }
+
+  // Write back results
+  c[row * N + col] = tmp;
+}
+
+// Check result on the CPU
+void verify_result(vector<int> &a, vector<int> &b, vector<int> &c) {
+  // For every row...
+  for (int i = 0; i < N; i++) {
+    // For every column...
+    for (int j = 0; j < N; j++) {
+      // For every element in the row-column pair
+      int tmp = 0;
+      for (int k = 0; k < N; k++) {
+        // Accumulate the partial results
+        tmp += a[i * N + k] * b[k * N + j];
+      }
+
+      // Check against the CPU result
+      assert(tmp == c[i * N + j]);
+    }
+  }
+}
+
+int main() {
+  // Size (in bytes) of matrix
+  size_t bytes = N * N * sizeof(int);
+
+  // Host vectors
+  vector<int> h_a(N * N);
+  vector<int> h_b(N * N);
+  vector<int> h_c(N * N);
+
+  // Initialize matrices
+  generate(h_a.begin(), h_a.end(), []() { return rand() % 100; });
+  generate(h_b.begin(), h_b.end(), []() { return rand() % 100; });
+
+  // Allocate device memory
+  int *d_a, *d_b, *d_c;
+  cudaMalloc(&d_a, bytes);
+  cudaMalloc(&d_b, bytes);
+  cudaMalloc(&d_c, bytes);
+
+  // Copy data to the device
+  cudaMemcpy(d_a, h_a.data(), bytes, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b, h_b.data(), bytes, cudaMemcpyHostToDevice);
+
+  // Threads per CTA dimension
+  int THREADS = 32;
+
+  // Blocks per grid dimension (assumes THREADS divides N evenly)
+  int BLOCKS = N / THREADS;
+
+  // Use dim3 structs for block  and grid dimensions
+  dim3 threads(THREADS, THREADS);
+  dim3 blocks(BLOCKS, BLOCKS);
+
+  // Launch kernel
+  matrixMul<<<blocks, threads>>>(d_a, d_b, d_c);
+
+  // Copy back to the host
+  cudaMemcpy(h_c.data(), d_c, bytes, cudaMemcpyDeviceToHost);
+
+  // Check result
+  verify_result(h_a, h_b, h_c);
+
+  cout << "COMPLETED SUCCESSFULLY\n";
+
+  // Free memory on device
+  cudaFree(d_a);
+  cudaFree(d_b);
+  cudaFree(d_c);
+
+  return 0;
 }
